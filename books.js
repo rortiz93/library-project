4f93979a513d9577c5630614cb56431b485e1e9d
rortiz93 <ortizryan76@yahoo.com>
2024-01-22 23:21:17 -0500

    modify book object from function to class

--- books.js
+++ books.js
@@ -58,17 +58,27 @@ addBookDialog.addEventListener("close", (event) => {
   
     
   });
-
-
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return (this.title + " by " + this.author + ", " + this.pages + " pages," + (this.read?" read":" not read yet"))
-    };
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    info() {
+        return (this.title + " by " + this.author + ", " + this.pages + " pages," + (this.read?" read":" not read yet"));
+    }
 }
+
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function(){
+//         return (this.title + " by " + this.author + ", " + this.pages + " pages," + (this.read?" read":" not read yet"))
+//     };
+// }
 
 function addBookToLibrary(book,rowNum){
 
